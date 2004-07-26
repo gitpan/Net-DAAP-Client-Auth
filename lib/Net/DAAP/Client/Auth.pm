721833de11e9215bc0515666dcdc34e6619ea5b4
Richard Clamp <richardc@unixbeard.net>
2004-07-26 19:14:45 +0000

    Import of RCLAMP/Net-DAAP-Client-Auth-1.21 from CPAN.
    gitpan-cpan-distribution: Net-DAAP-Client-Auth
    gitpan-cpan-version:      1.21
    gitpan-cpan-path:         RCLAMP/Net-DAAP-Client-Auth-1.21.tar.gz
    gitpan-cpan-author:       RCLAMP
    gitpan-cpan-maturity:     released

--- lib/Net/DAAP/Client/Auth.pm
+++ lib/Net/DAAP/Client/Auth.pm
@@ -1,55 +1,39 @@
 package Net::DAAP::Client::Auth;
 use strict;
-use warnings;
-use Net::DAAP::Client::Auth::Protocol::vAny;
-use Net::DAAP::Client::Auth::Protocol::v2;
-use Net::DAAP::Client::Auth::Protocol::v3;
-use base qw( Net::DAAP::Client );
-our $VERSION = '0.13';
+use Net::DAAP::Client 0.4;
+use vars qw( $VERSION @ISA );
+use Carp;
+$VERSION = 1.21;
+@ISA = qw( Net::DAAP::Client );
+
+sub new {
+    my $class = shift;
+    carp "Net::DAAP::Client::Auth->new deprecated in favour of Net::DAAP::Client->new";
+    $class->SUPER::new(@_);
+}
+
+1;
+__END__
 
 =head1 NAME
 
-Net::DAAP::Client::Auth - Extend Net::DAAP::Client to do iTunes authorisation
+Net::DAAP::Client::Auth - obsolete extension to Net::DAAP::Client
 
 =head1 SYNOPSIS
 
-  # see Net::DAAP::Client;
-  use Net::DAAP::Client::Auth;
-
+  See Net::DAAP::Client
 
 =head1 DESCRIPTION
 
-Subclasses Net::DAAP::Client and overrides methods to allow the module
-to provide suitable authentication tokens for iTunes 4.2 and 4.5.
 
-=cut
+Before the grand unification of Net::DAAP::Client and
+Net::DAAP::Client::Auth this distribution was a bag of slightly
+suspect hacks to force client validation into Net::DAAP::Client.
 
-
-# cheesy - rebless based on the iTunes reported version
-sub _do_get {
-    my $self = shift;
-
-    my $response = LWP::UserAgent->new->get(
-        "http://" . $self->{SERVER_HOST} . ":" . $self->{SERVER_PORT} .
-          "/server-info");
-
-    my $server = $response->headers->header('DAAP-Server') || '';
-    if ( $server =~ m{iTunes/4\.[56]} ) {
-        bless $self, __PACKAGE__."::Protocol::v3";
-    }
-    elsif ( $server =~ m{iTunes} ) {
-        bless $self, __PACKAGE__."::Protocol::v2";
-    }
-    else {
-        bless $self, __PACKAGE__."::Protocol::vAny";
-    }
-    $self->_do_get( @_ );
-}
-
-
-1;
-
-__END__
+As of release 0.4 of Net::DAAP::Client this code has been integrated
+and into the mainline, and you should use that.  This module is a
+mostly empty subclass to encourage you to upgrade and amend your
+scripts.
 
 =head1 AUTHOR
 
@@ -59,22 +43,11 @@ Richard Clamp <richardc@unixbeard.net>
 
 Copyright 2004 Richard Clamp.  All Rights Reserved.
 
-This program is free software; you can redistribute it
-and/or modify it under the same terms as Perl itself.
-
-
-=head1 BUGS
-
-None known.
-
-Bugs should be reported to me via the CPAN RT system.
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net::DAAP::Client::Auth>.
-
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-L<Net::DAAP::Client>,
-L<libopendaap|http://craz.net/programs/itunes/libopendaap.html> from
-which the auth code is adapted.
+Net::DAAP::Client
 
 =cut
